83874dfc154b80733059f0b8a881b38f67268f2d
Douglas McGee <dmg27i@gmail.com>
2018-07-02 13:06:52 +0100

    Get trades on dates and get totalled settled on a date and on a date for an entity, including tests.

--- src/main/java/com/dmg27/dtre/trade/Trades.java
+++ src/main/java/com/dmg27/dtre/trade/Trades.java
@@ -7,8 +7,14 @@
  */
 package com.dmg27.dtre.trade;
 
+import com.dmg27.dtre.core.BuySell;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * This class represents trades made.
@@ -34,17 +40,75 @@ final public class Trades {
         return tradesCltn;
     }
     
+    public List<Instruction> getTradesOn(LocalDate date) {
+        List<Instruction> result = this.tradesCltn.stream()
+            .filter(i -> date.equals(i.calculateEffectiveSettlementDate()))
+            .collect(Collectors.toList());
+        return result;
+    }
+    
+    public List<Instruction> getTradesSorted(List<Instruction> trades) {
+        List<Instruction> result = trades.stream()
+            .sorted()
+            .collect(Collectors.toList());
+        return result;
+    }
+    
     public Trades workingWeek(WorkingWeek workingWeek) {
         this.workingWeek = workingWeek;
         return this;
     }
 
     WorkingWeek getWorkingWeek() {
-        return workingWeek;
+        return this.workingWeek;
     }
     
     public void settle() {
-        this.tradesCltn.stream()
+        this.settle(this.tradesCltn);
+    }
+    
+    public void settleOn(LocalDate date) {
+        this.settle(this.getTradesOn(date));
+    }
+    
+    public void settle(List<Instruction> trades) {
+        trades.stream()
             .forEach(Instruction::settle);
     }
+    
+    public BigDecimal getTotalSettledIncommingOn(LocalDate date) {
+        return getTotalSettledOnAndFor(BuySell.S, date, Optional.empty());
+    }
+    
+    public BigDecimal getTotalSettledOutgoingOn(LocalDate date) {
+        return getTotalSettledOnAndFor(BuySell.B, date, Optional.empty());
+    }
+    
+    public BigDecimal getTotalSettledIncommingOnAndFor(LocalDate date, String entity) {
+        return getTotalSettledOnAndFor(BuySell.S, date, Optional.of(entity));
+    }
+    
+    public BigDecimal getTotalSettledOutgoingOnAndFor(LocalDate date, String entity) {
+        return getTotalSettledOnAndFor(BuySell.B, date, Optional.of(entity));
+    }
+    
+    BigDecimal getTotalSettledOnAndFor(BuySell buySell, LocalDate date, Optional<String> entity) {
+        List<BigDecimal> settlementAmounts = this.tradesCltn.stream()
+            .filter(i -> (date.equals(i.calculateEffectiveSettlementDate()) 
+                && (entity.isPresent() ? entity.get().equals(i.getEntity()) : true)
+                && i.getBuySell() == buySell))
+            .map(i -> i.getSettledAmount())
+            .filter(o -> o.isPresent())
+            .map(o -> o.get())
+            .collect(Collectors.toList());
+                
+        BigDecimal settled = new BigDecimal("0.00");
+        for (BigDecimal amount : settlementAmounts) {
+            settled = settled.add(amount);
+        }
+        
+        return settled;
+    }
+    
+    
 }
--- src/test/java/com/dmg27/dtre/trade/TradesTest.java
+++ src/test/java/com/dmg27/dtre/trade/TradesTest.java
@@ -7,15 +7,20 @@
  */
 package com.dmg27.dtre.trade;
 
+import com.dmg27.dtre.core.BuySell;
 import static com.dmg27.dtre.trade.InstructionTest.createInstruction;
 import static com.dmg27.dtre.trade.WorkingWeekConstants.DEFAULT_WORKING_WEEK;
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -55,42 +60,7 @@ public class TradesTest {
     }
     
     @Test
-    public void settlementTestUnsorted() {
-        Trades trades = new Trades()
-            .tradesCltn(this.createTrades())
-            .workingWeek(DEFAULT_WORKING_WEEK);
-        trades.settle();
-        
-        boolean[] expectedSettled = new boolean[] {
-            true,
-            true,
-            true,
-            false,
-            true,
-            true,
-            false,
-            true,
-            true,
-            true
-        };
-        String[] expectedSettledAmount = new String[] {
-            "13545.00",
-            "7525.00",
-            "16555.00",
-            "",
-            "4515.00",
-            "10025.00",
-            "",
-            "7518.75",
-            "15037.50",
-            "2506.25"
-        };
-        List<Instruction> tradesCltn = trades.getTradesCltn();
-        this.assertSettlement(tradesCltn, expectedSettled, expectedSettledAmount);
-    }
-    
-    @Test
-    public void settlementTestSorted() {
+    public void settlementTest() {
         Trades trades = new Trades()
             .tradesCltn(this.createTrades())
             .workingWeek(DEFAULT_WORKING_WEEK);
@@ -120,10 +90,7 @@ public class TradesTest {
             "",
             ""
         };
-        List<Instruction> tradesCltn = trades.getTradesCltn();
-        tradesCltn = tradesCltn.stream()
-            .sorted()
-            .collect(Collectors.toList());
+        List<Instruction> tradesCltn = trades.getTradesSorted(trades.getTradesCltn());
         this.assertSettlement(tradesCltn, expectedSettled, expectedSettledAmount);
     }
     
@@ -150,22 +117,103 @@ public class TradesTest {
         } 
     }
     
+    @Test
+    public void getTradesOnDateTest01() {
+        this.assertGetTradesOnDate("2016-01-07", new int[] {0, 1, 2, 4});
+    }
+    
+    @Test
+    public void getTotalSettledOutgoingOnAndFor01() {
+//        this.assertTotalSettledOnAndFor(BuySell.B, "2016-01-07", "bar", expectedAmount);
+    }
+    
+    @Test
+    public void getTradesOnDateTest02() {
+        this.assertGetTradesOnDate("2016-01-02", new int[] {});
+    }
+    
+    @Test
+    public void getTradesOnDateTest02a() {
+        this.assertGetTradesOnDate("2016-01-04", new int[] {5, 7, 9});
+    }
+    
+    @Test
+    public void getTradesOnDateTest02b() {
+        this.assertGetTradesOnDate("2016-01-03", new int[] {8});
+    }
+    
+    @Test
+    public void getTradesOnDateTest03() {
+        this.assertGetTradesOnDate("2016-02-06", new int[] {});
+    }
+    
+    @Test
+    public void getTradesOnDateTest03a() {
+        this.assertGetTradesOnDate("2016-02-08", new int[] {3});
+    }
+    
+    @Test
+    public void getTradesOnDateTest04() {
+        this.assertGetTradesOnDate("2016-03-05", new int[] {});
+    }
+    
+    @Test
+    public void getTradesOnDateTest04a() {
+        this.assertGetTradesOnDate("2016-03-07", new int[] {6});
+    }
+    
+    /**
+     * Assert the list of trades on a settlement date.
+     * @param settlementDate
+     * @param expectedIds 
+     */
+    private void assertGetTradesOnDate(String settlementDate, int[] expectedIds) {
+        Trades trades = new Trades()
+            .tradesCltn(this.createTrades())
+            .workingWeek(DEFAULT_WORKING_WEEK);
+        
+        List<Instruction> tradesOn = trades.getTradesOn(LocalDate.parse(settlementDate));
+        assertEquals(expectedIds.length, tradesOn.size());
+        for (int i = 0; i < tradesOn.size(); i++) {
+            assertEquals(expectedIds[i], tradesOn.get(i).getId());
+        }
+    }
+    
+    /**
+     * Assert the amount settled on a date for an optional entity.
+     * @param buySell
+     * @param settlementDate
+     * @param entity Optional
+     * @param expectedAmount 
+     */
+    private void assertTotalSettledOnAndFor(BuySell buySell, String settlementDate, Optional<String> entity, String expectedAmount) {
+        Trades trades = new Trades()
+            .tradesCltn(this.createTrades())
+            .workingWeek(DEFAULT_WORKING_WEEK);
+        
+        BigDecimal amount = trades.getTotalSettledOnAndFor(buySell, LocalDate.parse(settlementDate), entity);
+        assertEquals(expectedAmount, amount);
+    }
+    
     /**
      * Create a collection of trade instructions.
+     * <p>
+     * Don't change unless you really know what you are doing.
+     * </p>
      * @return The trade instructions.
      */
     private List<Instruction> createTrades() {
         List<Instruction> trades = new ArrayList<>();
-        trades.add(createInstruction("barS", "S", "0.2", "AED", "05 Jan 2016", "07 Jan 2016", 450, "150.5"));   // sunday to thursday
-        trades.add(createInstruction("barS", "S", "0.2", "ALL", "05 Jan 2016", "07 Jan 2016", 250, "150.5"));
-        trades.add(createInstruction("barS", "S", "0.2", "BAM", "05 Jan 2016", "07 Jan 2016", 550, "150.5"));
-        trades.add(createInstruction("barS", "S", "0.2", "BBD", "01 Feb 2016", "02 Feb 2016", 650, "150.5"));   // unsettleable
-        trades.add(createInstruction("barS", "S", "0.2", "CAD", "05 Jan 2016", "07 Jan 2016", 150, "150.5"));
-        trades.add(createInstruction("fooB", "B", "0.50", "CDF", "01 Jan 2016", "02 Jan 2016", 200, "100.25"));
-        trades.add(createInstruction("fooB", "B", "0.50", "ETB", "01 Mar 2016", "02 Mar 2016", 100, "100.25"));   // unsettleable
-        trades.add(createInstruction("fooB", "B", "0.50", "EUR", "01 Jan 2016", "02 Jan 2016", 150, "100.25"));
-        trades.add(createInstruction("fooB", "B", "0.50", "SAR", "01 Jan 2016", "02 Jan 2016", 300, "100.25"));   // sunday to thursday
-        trades.add(createInstruction("fooB", "B", "0.50", "SGD", "01 Jan 2016", "02 Jan 2016", 50, "100.25"));
+        trades.add(createInstruction("bar", "S", "0.2",  "AED", "05 Jan 2016", "07 Jan 2016", 450, "150.5"));    // 0;THU;sunday to thursday;OK
+        trades.add(createInstruction("bar", "B", "0.2",  "ALL", "05 Jan 2016", "07 Jan 2016", 250, "150.5"));    // 1;THU;OK
+        trades.add(createInstruction("bar", "B", "0.2",  "BAM", "05 Jan 2016", "07 Jan 2016", 550, "150.5"));    // 2;THU;OK
+        trades.add(createInstruction("bar", "S", "0.2",  "BBD", "01 Feb 2016", "06 Feb 2016", 650, "150.5"));    // 3;SAT;to MON 08 Feb 2016;future;unsettleable
+        trades.add(createInstruction("bar", "S", "0.2",  "CAD", "05 Jan 2016", "07 Jan 2016", 150, "150.5"));    // 4;THU;OK
+        trades.add(createInstruction("foo", "B", "0.50", "CDF", "01 Jan 2016", "02 Jan 2016", 200, "100.25"));   // 5;SAT;to MON 04 Jan 2016
+        trades.add(createInstruction("foo", "S", "0.50", "ETB", "01 Mar 2016", "05 Mar 2016", 100, "100.25"));   // 6;SAT;future;unsettleable
+        trades.add(createInstruction("foo", "S", "0.50", "EUR", "01 Jan 2016", "02 Jan 2016", 150, "100.25"));   // 7;SAT;to MON 04 Jan 2016
+        trades.add(createInstruction("foo", "B", "0.50", "SAR", "01 Jan 2016", "02 Jan 2016", 300, "100.25"));   // 8;SAT;sunday to thursday;to SUN 03 Jan 2016 
+        trades.add(createInstruction("foo", "B", "0.50", "SGD", "01 Jan 2016", "02 Jan 2016",  50, "100.25"));   // 9;SAT;to MON 04 Jan 2016
         return trades;
     }
 }
